e43732a760984cdd1c87b86c0be9ecac86161a16
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2024-11-28 16:22:54 +0530

    Delete __pycache__/ai_powered_backend directory

--- __pycache__/ai_powered_backend/app/services/summarization_services.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-
-def summarize_transcript(transcript):
-    summary = summarizer(transcript, max_length=130, min_length=30, do_sample=False)
-    return summary[0]['summary_text']
